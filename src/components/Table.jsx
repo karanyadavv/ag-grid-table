a8b717567503a67dc00e867164ab11803ef0cdf3
karanyadavv <karanyadav729@gmail.com>
2025-08-25 23:50:34 +0530

    cleaning up comments

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,7 +3,7 @@ import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 import { themeQuartz } from "ag-grid-community";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
-import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import { AgGridReact } from "ag-grid-react";
 import data from "../data/data.json";
 
 const AgGridTable = () => {
@@ -13,16 +13,6 @@ const AgGridTable = () => {
     field: item,
   }));
   const [colDefs, setColDefs] = useState(cols);
-
-  // const myTheme = themeQuartz.withParams({
-  //   backgroundColor: "oklch(43.9% 0 0)",
-  //   foregroundColor: "oklch(92.2% 0 0)",
-  //   headerTextColor: "oklch(96.7% 0.001 286.375)",
-  //   headerBackgroundColor: "oklch(26.9% 0 0)",
-  //   oddRowBackgroundColor: "oklch(37.1% 0 0)",
-  //   headerColumnResizeHandleColor: "oklch(96.7% 0.001 286.375)",
-  //   borderColor:
-  // });
 
   const myTheme = themeQuartz.withParams({
     backgroundColor: "#232529ff",
