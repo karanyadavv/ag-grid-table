e9d960aca18ed1107877c07bb4bf86c5d6361a9b
karanyadavv <karanyadav729@gmail.com>
2025-08-25 23:07:03 +0530

    starter ag-grid table

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import AgGridTable from "./components/Table";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>Ag-grid table</div>
+      <AgGridTable />
     </>
   );
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+ModuleRegistry.registerModules([AllCommunityModule]);
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+
+const AgGridTable = () => {
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
+  return (
+    <div style={{ height: 500 }}>
+      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    </div>
+  );
+};
+
+export default AgGridTable;
